6f1a0ac394066285b4e962b8458527c28e2bbefc
Serhii_Paukov <spspider95@gmail.com>
2023-10-15 15:57:46 +0300

    create files bash

--- main_create_files_bash.sh
+++ main_create_files_bash.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
 folder="ProgramToSend"
-chunksize=100
-
+splitted_folder="./splitted/"
+chunksize=800
 
 if ! command -v qrencode &>/dev/null; then
   # install qrencode if it is not installed
   sudo apt-get update
   sudo apt-get install -y qrencode
 fi
-
-# loop over all files in the folder and its subdirectories
 for each_file in "$folder"/* "$folder"/*/*; do
-  # check if the current item is a file
   if [[ -f "$each_file" ]]; then
     # split the file into 100-character chunks and store them in an array
-    chunks=()
-    while IFS= read -r -n $chunksize chunk; do
-      chunks+=("$chunk")
-    done < "$each_file"
-    num_chunks=${#chunks[@]}
-
+    rm -rf "$splitted_folder"
+    mkdir -p "$splitted_folder"
+    split --numeric-suffixes=1 --suffix-length=3 -d --additional-suffix=.chunk --verbose "$each_file" -C "$chunksize" "$splitted_folder" &>/dev/null
+    num_chunks=$(ls -1 "$splitted_folder" | wc -l)
+    i=0
     # generate a QR code from each chunk and wait for user input
-
-    for i in "${!chunks[@]}"; do
+    for each_splitted_file in "$splitted_folder"* "$splitted_folder"*/*; do
+      json="{\"p\": \"$i\", \"a\": $(($num_chunks)), \"f\": \"$each_file\"}"
+      ((i++))
       clear
-      echo "Processing file: $each_file"
-      echo "number: $((i+1)) of $num_chunks:"
-      echo "${chunks[i]}" | qrencode -t ansiutf8
-      # wait for user input of "s" to skip file or any other key to continue
+      echo -e "$json\n&&&&&&&&&&&&777777777777\n$(cat $each_splitted_file)" | qrencode -t ansiutf8
       while true; do
-        echo "Press 's' to skip file, any other key to continue"
+        echo "$each_file part:$i of: $num_chunks"
         read -n1 input
         if [[ $input == "s" ]]; then
           break 2
@@ -40,5 +34,6 @@ for each_file in "$folder"/* "$folder"/*/*; do
         sleep 1
       done
     done
+    rm -rf "$splitted_folder"
   fi
 done
