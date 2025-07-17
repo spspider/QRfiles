006e2b7a2571afd6bc0e19c2f6653613b3f30c6e
Serhii_Paukov <spspider95@gmail.com>
2025-07-17 18:03:54 +0300

    auto update from EPAM computer

--- readcontent.py
+++ readcontent.py
@@ -1,16 +1,27 @@
 import os
+import pyperclip
+
 
 def iterate_folders_and_print_content(target_folder):
+    all_output = []  # List to store all output
     for root, dirs, files in os.walk(target_folder):
         for file in files:
             file_path = os.path.join(root, file)
             relative_path = os.path.relpath(file_path, target_folder)
             with open(file_path, 'r') as f:
                 file_content = f.read()
-            print(f"file: {relative_path}, content: \"\"\"{file_content}\"\"\"")
+            output = f"file: {relative_path}, content: \"\"\"{file_content}\"\"\""
+            print(output)
+            all_output.append(output)  # Append the output to the list
+
+    # Combine all output into a single string and copy to clipboard
+    combined_output = "\n".join(all_output)
+    pyperclip.copy(combined_output)
+    print("\nAll output has been copied to the clipboard.")
+
 
 # Specify the folder to run the script in
-target_folder = "recieved/mlr_serhiipaukovmicro"
+target_folder = "recieved/project"
 
 # Call the function
 iterate_folders_and_print_content(target_folder)
---
+++ start_write.bat
@@ -0,0 +1,4 @@
+@echo off
+call venv\Scripts\activate.bat
+python main_keypress.py
+
